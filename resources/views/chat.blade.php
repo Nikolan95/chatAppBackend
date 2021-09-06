df50c17807616a39c12e9c47acbbf91dfec3bc60
Armin Hamzic <arminhamzic01@gmail.com>
2021-09-06 16:44:58 +0200

    removing these script files

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -30,38 +30,8 @@
     <!-- App styles -->
     <link rel="stylesheet" href="{{asset('css/appold.css')}}">
 
-    <!-- /Main Wrapper -->
-    <!-- jQuery -->
-    <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
+   
 
-    <!-- Bootstrap Core JS -->
-    <script src="{{asset('/js/popper.min.js')}}"></script>
-    <script src="{{asset('/js/bootstrap.min.js')}}"></script>
-
-    <!-- Custom Scroll JS -->
-    <script src="{{asset('/js/jquery.nicescroll.min.js')}}"></script>
-    <script src="{{asset('/plugins/mcustomscroll/jquery.mCustomScrollbar.js')}}"></script>
-
-    <script src="{{asset('/js/custom.js')}}"></script>
-
-    <script src="{{asset('/js/images.js')}}"></script>
-
-    <script src="{{asset('/js/chatgroup.js')}}"></script>
-
-    <script src="{{asset('/js/accounting.js')}}"></script>
-
-    <script src="{{asset('/js/offerform.js')}}"></script>
-
-    <script src="{{asset('metrica/js/jquery.min.js')}}"></script>
-    <script src="{{asset('metrica/js/jquery-ui.min.js')}}"></script>
-    <script src="{{asset('metrica/js/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{asset('metrica/js/metismenu.min.js')}}"></script>
-    <script src="{{asset('metrica/js/waves.js')}}"></script>
-    <script src="{{asset('metrica/js/feather.min.js')}}"></script>
-    <script src="{{asset('metrica/js/jquery.slimscroll.min.js')}}"></script>
-    <script src="{{asset('metrica/js/plugins/apexcharts/apexcharts.min.js')}}"></script>
-    <script src="{{asset('metrica/js/app.js')}}"></script>
-    <script src="{{asset('/js/script.js')}}"></script>
 
 </head>
 
